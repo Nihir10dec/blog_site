46061b42688f02dc3b918fb3ff602705ea902433
Nihir Shah <nihir10dec@gmail.com>
2020-07-11 19:16:28 +0530

    cbv and models

---
+++ blog/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from blog.models import Post,Comments
+
+class PostForm(forms.ModelForm):
+
+    class Meta():
+        model = Post
+        fields = ('author' , 'title' , 'text')
+
+        widgets = {
+            'author': forms.TextInput(attrs={'class':'form-control'}),
+            'title': forms.TextInput(attrs={'class':'form-control textinputclass'}),
+            'text': forms.Textarea(attrs={'class':'form-control editable medium-editor-text-area postcontent'}),
+        }
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta():
+        model = Comments
+        fields = ('author' , 'text')
+
+        widgets = {
+            'author': forms.TextInput(attrs={'class':'form-control'}),
+            'text': forms.Textarea(attrs={'class':'form-control editable medium-editor-text-area'}),
+        }
--- blog_site/settings.py
+++ blog_site/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR , 'blog/templates/blog')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'blog_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'blog_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
     }
 }
 
@@ -119,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR , 'static')
+
+LOGIN_REDIRECT_URL = '/'
